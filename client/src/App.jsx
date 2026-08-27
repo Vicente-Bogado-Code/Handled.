d19d384726766eac81d9223f16beb2abdf3ca232
Vicente <bogadovincenzo4@gmail.com>
2026-08-27 23:26:59 +0200

    ...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,23 @@ import Header from "./features/header/header";
 import HomePage from "./features/home-page/homePageMain";
 import AuthMainPortal from "./features/auth/authPortal";
 import CurrentProjectComp from "./features/onProject/onProject";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { logout } from "./features/api/authRequests/auth";
+import { getMyData } from "./features/api/getDataRequests/getWhoAmI";
 
 export default function App(){
     const [user,setUser] = useState(null)
     const [currentId, setCurrentId] = useState("")
+    useEffect(() => {
+    async function getCookie() {
+        const r = await getMyData()
+        if (r.Status === "Data retrieved"){
+            setUser(r.Me.username)
+        }
+    }
+    getCookie();
+
+    }, [])
     function handleLogout(){
         logout()
         setUser(null)
